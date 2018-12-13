cfdda7616cce3dd4a5e2634afcec539d45bd01a7
baobao98 <lamphuocbao98@gmail.com>
2018-12-13 14:55:59 +0700

    i

--- real_estate/real_estate/frmLogin.cs
+++ real_estate/real_estate/frmLogin.cs
@@ -74,12 +74,12 @@ namespace real_estate
             string pass = txtPass.Text;
 
 
-            //int kt = TaiKhoanBAL.CheckAcc(txtUser.Text, txtPass.Text);
-            //if ( TaiKhoanBAL.CheckAcc(txtUser.Text,txtPass.Text)==1)
-            //{
-            //    account a = TaiKhoanBAL.GetAccountByAcc(txtUser.Text);
-            //    int? quyen=a.type_ac;
-            //}
+            int kt = TaiKhoanBAL.CheckAcc(txtUser.Text, txtPass.Text);
+            if (TaiKhoanBAL.CheckAcc(txtUser.Text, txtPass.Text) == 1)
+            {
+                account a = TaiKhoanBAL.GetAccountByAcc(txtUser.Text);
+                int? quyen = a.type_ac;
+            }
             Form frm = new FormRS();
             frm.Show();
         }
